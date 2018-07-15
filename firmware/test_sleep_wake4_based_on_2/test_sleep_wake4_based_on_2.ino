d334edf9db0ab82ae676be662fdf961d001de468
Oren Lederman <oren.lederman@gmail.com>
2018-07-14 21:17:37 -0400

    changed interrupt pins to 0 and 2

--- firmware/test_sleep_wake4_based_on_2/test_sleep_wake4_based_on_2.ino
+++ firmware/test_sleep_wake4_based_on_2/test_sleep_wake4_based_on_2.ino
@@ -1,11 +1,11 @@
 #include <avr/sleep.h>
 #include <avr/interrupt.h>
 
-const int switchPin                     = 3;
-const int switchPin2                     = 4;
+const int switchPin                     = 0;
+const int switchPin2                     = 2;
 
 int awakeLedPin = 1;            // LED connected to digital pin 0. Shows when the circuit is awake
-int interruptLedPin = 2;      // LED to show the action of a interrupt
+int interruptLedPin = 3;      // LED to show the action of a interrupt
 
 void setup() {
 
@@ -14,7 +14,9 @@ void setup() {
   pinMode(switchPin2, INPUT_PULLUP);
   pinMode(awakeLedPin, OUTPUT);         // sets the digital pin as output
   pinMode(interruptLedPin, OUTPUT);   // sets the digital pin as output
-    
+
+
+  /*
 
     // Flash quick sequence so we know setup has started
     for (int k = 0; k < 10; k = k + 1) {
@@ -26,13 +28,15 @@ void setup() {
             }
         delay(250);
         } // for
+        */
+        delay(500);
     } // setup
 
 void sleep() {
 
     GIMSK |= _BV(PCIE);                     // Enable Pin Change Interrupts
-    PCMSK |= _BV(PCINT3);                   // Use PB3 as interrupt pin
-    PCMSK |= _BV(PCINT4);                   // Use PB4 as interrupt pin
+    PCMSK |= _BV(PCINT0);                   // Use PB3 as interrupt pin
+    PCMSK |= _BV(PCINT2);                   // Use PB4 as interrupt pin
     ADCSRA &= ~_BV(ADEN);                   // ADC off
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // replaces above statement
     digitalWrite(awakeLedPin, LOW); 
@@ -42,8 +46,8 @@ void sleep() {
     sleep_cpu();                            // sleep
 
     cli();                                  // Disable interrupts
-    PCMSK &= ~_BV(PCINT3);                  // Turn off PB3 as interrupt pin
-    PCMSK &= ~_BV(PCINT4);                  // Turn off PB4 as interrupt pin
+    PCMSK &= ~_BV(PCINT0);                  // Turn off PB3 as interrupt pin
+    PCMSK &= ~_BV(PCINT2);                  // Turn off PB4 as interrupt pin
     sleep_disable();                        // Clear SE bit
     ADCSRA |= _BV(ADEN);                    // ADC on
     sei();                                  // Enable interrupts
