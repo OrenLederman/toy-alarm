711842f300c03c61d025fceb1a7ac8f53ddb3c1b
Oren Lederman <oren.lederman@gmail.com>
2018-07-14 21:36:14 -0400

    Added a second LED for the second switch

--- firmware/test_sleep_wake4_based_on_2/test_sleep_wake4_based_on_2.ino
+++ firmware/test_sleep_wake4_based_on_2/test_sleep_wake4_based_on_2.ino
@@ -1,3 +1,5 @@
+#include "PinChangeInterrupt.h"
+
 #include <avr/sleep.h>
 #include <avr/interrupt.h>
 
@@ -6,37 +8,28 @@ const int switchPin2                     = 2;
 
 int awakeLedPin = 1;            // LED connected to digital pin 0. Shows when the circuit is awake
 int interruptLedPin = 3;      // LED to show the action of a interrupt
+int interruptLedPin2 = 4;      // LED to show the action of a interrupt
 
 void setup() {
 
   pinMode(switchPin, INPUT);
-  digitalWrite(switchPin, LOW); 
-  pinMode(switchPin2, INPUT_PULLUP);
+  pinMode(switchPin2, INPUT);
+  
   pinMode(awakeLedPin, OUTPUT);         // sets the digital pin as output
   pinMode(interruptLedPin, OUTPUT);   // sets the digital pin as output
+  pinMode(interruptLedPin2, OUTPUT);   // sets the digital pin as output
 
-
-  /*
-
-    // Flash quick sequence so we know setup has started
-    for (int k = 0; k < 10; k = k + 1) {
-        if (k % 2 == 0) {
-            digitalWrite(awakeLedPin, HIGH);
-            }
-        else {
-            digitalWrite(awakeLedPin, LOW);
-            }
-        delay(250);
-        } // for
-        */
-        delay(500);
-    } // setup
+  delay(500);
+  
+} // setup
 
 void sleep() {
 
-    GIMSK |= _BV(PCIE);                     // Enable Pin Change Interrupts
-    PCMSK |= _BV(PCINT0);                   // Use PB3 as interrupt pin
-    PCMSK |= _BV(PCINT2);                   // Use PB4 as interrupt pin
+    //GIMSK |= _BV(PCIE);                     // Enable Pin Change Interrupts
+    attachPCINT(digitalPinToPCINT(switchPin), blinkLed, CHANGE);
+    attachPCINT(digitalPinToPCINT(switchPin2), blinkLed2, CHANGE);
+
+
     ADCSRA &= ~_BV(ADEN);                   // ADC off
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);    // replaces above statement
     digitalWrite(awakeLedPin, LOW); 
@@ -46,24 +39,29 @@ void sleep() {
     sleep_cpu();                            // sleep
 
     cli();                                  // Disable interrupts
-    PCMSK &= ~_BV(PCINT0);                  // Turn off PB3 as interrupt pin
-    PCMSK &= ~_BV(PCINT2);                  // Turn off PB4 as interrupt pin
+    detachPCINT(digitalPinToPCINT(switchPin));
+    detachPCINT(digitalPinToPCINT(switchPin2));
     sleep_disable();                        // Clear SE bit
     ADCSRA |= _BV(ADEN);                    // ADC on
     sei();                                  // Enable interrupts
     
     delay(500);                           // wait 2 sec. so humans can notice the interrupt LED to show the interrupt is handled
     digitalWrite (interruptLedPin, LOW);       // turn off the interrupt LED
+    digitalWrite (interruptLedPin2, LOW);       // turn off the interrupt LED
 
     
     } // sleep
 
-ISR(PCINT0_vect) {
-    // This is called when the interrupt occurs, but I don't need to do anything in it  
-  //execute code here after wake-up before returning to the loop() function
-  // timers and code using timers (serial.print and more...) will not work here.
+void blinkLed(void) {
+  // Switch Led state
   digitalWrite(interruptLedPin, HIGH);
- }
+}
+
+void blinkLed2(void) {
+  // Switch Led state
+  digitalWrite(interruptLedPin2, HIGH);
+}
+
 
 void loop() {
     digitalWrite(awakeLedPin, HIGH);            // sets the LED on
